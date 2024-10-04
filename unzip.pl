51c576fd7dbd67fddb2eb38f5d8865ccd179f38d
xMakuno <mamv2001@gmail.com>
2024-10-03 20:44:19 -0600

    👍add: homework assignment

--- unzip.pl
+++ unzip.pl
@@ -1,1 +1,12 @@
-unzip(L,R1,R2):-!.
+% unzip(ZippedList, List1, List2, ReturnValue1, ReturnValue2).
+% unzip([ [3,7], [2,6], [1,5] ]).
+% should return:
+% R1 = [3,2,1]
+% R2 = [7,6,5]
+unzip([ [ R1, R2 ] | []], R1, R2):-!.
+unzip([[L1, L2] | T], R1,R2):-
+    unzip(T, R1, R2),
+    =(R1, [L1 | R1]),
+    =(R2, [L2 | R2]).
+unzip(L):-
+    unzip(L, [], []).
---
+++ unzip2.pl
@@ -0,0 +1,10 @@
+% Base case: empty list results in two empty lists.
+unzip([], [], []).
+
+% Recursive case: split head and recurse on the tail.
+unzip([[L1, L2] | T], [L1 | R1], [L2 | R2]) :-
+    unzip(T, R1, R2).
+unzipMain(L):-
+unzip(L, R1, R2),
+write('R1: '), writeln(R1),
+write('R2: '), writeln(R2).
