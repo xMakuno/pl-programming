a4fbb75e59dbf0b1602b3c483603cc2cef7c9208
xMakuno <mamv2001@gmail.com>
2024-10-24 06:36:24 -0600

    🎨update: code structure for unzip

--- unzip2.pl
+++ unzip2.pl
@@ -4,7 +4,8 @@ unzip([], [], []).
 % Recursive case: split head and recurse on the tail.
 unzip([[L1, L2] | T], [L1 | R1], [L2 | R2]) :-
     unzip(T, R1, R2).
+
 unzipMain(L):-
-unzip(L, R1, R2),
-write('R1: '), writeln(R1),
-write('R2: '), writeln(R2).
+    unzip(L, R1, R2),
+    write('R1: '), writeln(R1),
+    write('R2: '), writeln(R2).
