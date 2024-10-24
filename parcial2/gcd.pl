e6506fe6aa7948fe5d944dce6c3c242ab8e3d4be
xMakuno <mamv2001@gmail.com>
2024-10-24 07:39:02 -0600

    👍add: both versions of gcd

--- parcial2/gcd.pl
+++ parcial2/gcd.pl
@@ -1,3 +1,6 @@
-% gcd()
-gcd(A, 0) :- write(A),!.
-gcd(A, B) :- is(R, mod(A,B)), gcd(B, R).
+% GCD Version that returns value
+gcd(A, 0, A):-!.
+gcd(A, B, D) :- is(R, mod(A,B)), gcd(B, R, D).
+% GCD Version that prints out to console
+gcd2(A, 0) :- write(A),!.
+gcd2(A, B) :- is(R, mod(A,B)), gcd2(B, R).
